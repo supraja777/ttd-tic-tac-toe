cc7a1a0714e0595691f3053c1f9d0683e9eb5f70
supraja777 <ssupraja777@gmail.com>
2023-12-27 10:07:52 +0530

    tic-tac-toe with ttd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Board />;
 }
+
+export const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xisNext, setXIsNext] = useState(true);
+  let status;
+  const handleClick = (index) => {
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
+
+    const newSquares = squares.slice();
+    if (xisNext) {
+      newSquares[index] = "X";
+    } else {
+      newSquares[index] = "O";
+    }
+    setXIsNext(!xisNext);
+    setSquares(newSquares);
+  };
+  const winner = calculateWinner(squares);
+  if (winner) {
+    status = "winner: " + winner;
+  } else {
+    status = "Next player: " + (xisNext ? "X" : "O");
+  }
+  return (
+    <>
+      <div data-testid="status">{status}</div>
+      <div>
+        <div>
+          <div data-testid="gameBoard">
+            <Square value={squares[0]} handleClick={() => handleClick(0)} />
+            <Square value={squares[1]} handleClick={() => handleClick(1)} />
+            <Square value={squares[2]} handleClick={() => handleClick(2)} />
+          </div>
+          <Square value={squares[3]} handleClick={() => handleClick(3)} />
+          <Square value={squares[4]} handleClick={() => handleClick(4)} />
+          <Square value={squares[5]} handleClick={() => handleClick(5)} />
+        </div>
+        <Square value={squares[6]} handleClick={() => handleClick(6)} />
+        <Square value={squares[7]} handleClick={() => handleClick(7)} />
+        <Square value={squares[8]} handleClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+};
+export const Square = ({ value, handleClick }) => {
+  return <button onClick={handleClick}>{value}</button>;
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,56 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App",()=>{
+  it("should be X",()=>{
+      render(<App/>);
+      const gameBoard=screen.getByTestId("gameBoard");
+      expect(gameBoard).not.toBeNull();
+      expect(gameBoard.textContent).toBe("");
+  });
+
+  it("number of buttons are 9",()=>{
+    render(<App/>);
+    const buttons=screen.getAllByRole("button");
+    expect(buttons.length).toBe(9);
+  });
+
+  it("should render X on first click",()=>{
+    render(<App/>);
+    const button =screen.getAllByRole("button")[0];
+    fireEvent.click(button);
+    expect(button.textContent).toBe("X");
+  });
+
+it("should render O on the second move",()=>{
+    render(<App/>);
+    const Square=screen.getAllByRole("button");
+    fireEvent.click(Square[0]);
+    expect(Square[0].textContent).toBe("X");
+    fireEvent.click(Square[1]);
+    expect(Square[1].textContent).toBe("O");
+});
+
+it("should display token on the square only when it is empty",()=>{
+  render(<App/>);
+  const square=screen.getAllByRole("button");
+  fireEvent.click(square[0]);
+  expect(square[0].textContent).toBe("X");
+  fireEvent.click(square[0]);
+  expect(square[0].textContent).toBe("X");
+});
+
+it("should be able to calculate winner",()=>{
+  render(<App/>);
+  const squares=screen.getAllByRole("button");
+  const status=screen.getByTestId("status");
+  fireEvent.click(squares[0]);
+  fireEvent.click(squares[1]);
+  fireEvent.click(squares[3]);
+  fireEvent.click(squares[4]);
+  fireEvent.click(squares[6]);
+  fireEvent.click(squares[7]);
+  expect(status.textContent).toBe("winner: X");
+});
+
 });
